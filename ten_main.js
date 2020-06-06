2f468b470c24bc5a05a3c34ed1b35fd24237fa43
FeX Uranus <66378306+FeX-Uranus@users.noreply.github.com>
2020-06-06 14:29:07 +0900

    Add files via upload

---
+++ ten_main.js
@@ -0,0 +1,297 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  60,
+  window.innerWidth / window.innerHeight,
+  1,
+  10000
+);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0xaaaaaa, 1);
+
+const canvas = $('#canvascontainer').append(renderer.domElement);
+let distance = 4;
+let started = false;
+let percent = 0;
+camera.position.set(0, 5, distance);
+camera.rotation.x -= 0.75;
+let scoreSubmitted = false;
+let level = 1;
+let star = 0;
+let data;
+let reqId;
+$.getJSON('ten_levels.json', d => {
+  data = d;
+  loadLevel(level);
+  $('#play').hide();
+  $('#PresentCode').show();
+  $('#play').click(start);
+  $('#next').show();
+  $('#score').hide();
+  if (level == 1) {
+      $('#level-d').html('開催日　9月21日');
+      $('#level').html('The Ruby');
+      $('#stars').html('☆☆☆☆☆☆☆☆☆☆');
+     } else if (level == 2) {
+      $('#level-d').html('開催日　3月4日');
+      $('#level').html('The Beryl');
+      $('#stars').html('☆☆☆☆☆☆☆☆☆☆');
+     } else if (level == 3) {
+      $('#level-d').html('開催日　1月1日');
+      $('#level').html('The Dia');
+      $('#stars').html('☆☆☆☆☆☆☆☆☆☆');
+     } else if (level == 4) {
+      $('#level-d').html('開催日　8月1日');
+      $('#level').html('The Kyanite');
+      $('#stars').html('☆☆☆☆☆☆☆☆☆☆');
+     } else {
+            $('#level-d').html('開発中');
+            $('#level').html('Level ' + level);
+            $('#stars').html('');
+           }
+  reqId = requestAnimationFrame(render);
+  console.clear();
+  console.log(
+    "超高難度 - The Ruby　血祭りの時間だ"
+  );
+});
+
+//start function
+function start(e) {
+  e.preventDefault();
+  if (!started) {
+    started = true;
+    ball.speed.z = -0.15;
+    $('#main').fadeOut(300);
+    $('#name').hide();
+    if (level == 1) {
+        $('#ruby').get(0).play();
+        } else if (level == 2) {
+        $('#rgw').get(0).play();
+        } else if (level == 3) {
+        $('#dia').get(0).play();
+        } else if (level == 4) {
+        $('#kya').get(0).play();
+        } else {
+            $('#cloud').get(0).play();
+            }
+    reset();
+    world.forEach(v => {
+      if (v instanceof Bouncer) {
+        v.line.position.y = 0;
+      }
+    });
+    world.forEach(v => {
+      if (v instanceof Mysterybox) {
+        v.mesh.position.y = 1.1;
+      }
+    });
+    world.forEach(v => {
+      if (v instanceof Gem) {
+        v.mesh.position.y = 1.1;
+      }
+    });
+    $('#main').css('pointer-events', 'none');
+  }
+}
+function reset() {
+  ball.landed = true;
+  ball.tmpZ = 0;
+  camera.position.set(0, 5, distance);
+  ball.mesh.position.set(0, 0.6, 0);
+  ball.speed.y = 0;
+  ball.count2Lose = 0;
+}
+
+function nextLevel() {
+  percent = 0;
+  star = 0;
+  while (
+    (selectedObject = scene.getObjectByName('level component')) !== undefined
+  ) {
+    scene.remove(selectedObject);
+  }
+  world.length = 0;
+  level++;
+  loadLevel(level);
+  reset();
+  $('#score').hide();
+  $('#level').show();
+  $('#stars').show();
+  $('#prev').show();
+  $('#retry').hide();
+  $('#play').show();
+  if (level == 3) {
+    $('#next').hide();
+  }
+  if (level == 1) {
+      $('#level-d').html('開催日　9月21日');
+      $('#level').html('The Ruby');
+      $('#stars').html('☆☆☆☆☆☆☆☆☆☆');
+     } else if (level == 2) {
+      $('#level-d').html('開催日　3月4日');
+      $('#level').html('The Beryl');
+      $('#stars').html('☆☆☆☆☆☆☆☆☆☆');
+     } else if (level == 3) {
+      $('#level-d').html('開催日　1月1日');
+      $('#level').html('The Dia');
+      $('#stars').html('☆☆☆☆☆☆☆☆☆☆');
+     } else if (level == 4) {
+      $('#level-d').html('開催日　8月1日');
+      $('#level').html('The Kyanite');
+      $('#stars').html('☆☆☆☆☆☆☆☆☆☆');
+     } else {
+            $('#level-d').html('開発中');
+            $('#level').html('Level ' + level);
+            $('#stars').html('');
+           }
+}
+
+function prevLevel() {
+  percent = 0;
+  star = 0;
+  while (
+    (selectedObject = scene.getObjectByName('level component')) != undefined
+  ) {
+    scene.remove(selectedObject);
+  }
+  world.length = 0;
+  level--;
+  loadLevel(level);
+  reset();
+  $('#score').hide();
+  $('#level').show();
+  $('#stars').show();
+  $('#next').show();
+  $('#retry').hide();
+  $('#play').show();
+  if (level == 1) {
+    $('#prev').hide();
+  }
+  if (level == 1) {
+      $('#level-d').html('開催日　9月21日');
+      $('#level').html('The Ruby');
+      $('#stars').html('☆☆☆☆☆☆☆☆☆☆');
+     } else if (level == 2) {
+      $('#level-d').html('開催日　3月4日');
+      $('#level').html('The Beryl');
+      $('#stars').html('☆☆☆☆☆☆☆☆☆☆');
+     } else if (level == 3) {
+      $('#level-d').html('開催日　1月1日');
+      $('#level').html('The Dia');
+      $('#stars').html('☆☆☆☆☆☆☆☆☆☆');
+     } else if (level == 4) {
+      $('#level-d').html('開催日　8月1日');
+      $('#level').html('The Kyanite');
+      $('#stars').html('☆☆☆☆☆☆☆☆☆☆');
+     } else {
+            $('#level-d').html('開発中');
+            $('#level').html('Level ' + level);
+            $('#stars').html('');
+           }
+}
+
+const light = new THREE.HemisphereLight(0xeeeeee, 0x777777);
+scene.add(light);
+const world = [];
+function loadLevel(level) {
+  const index = level - 1;
+  renderer.setClearColor(parseInt(data[index].background));
+  ball.mesh.material.color.setHex(parseInt(data[index].ball));
+  scene.fog = new THREE.Fog(0x009A48, 5, 10);
+  for (var i in data[index].data) {
+    for (var j in data[index].data[i]) {
+      switch (data[index].data[i][j]) {
+        case 1:
+          world.push(new Mat(j - 2, -i, data[index].mat));
+          break;
+        case 2:
+          world.push(new Bouncer(j - 2, -i, data[index].bouncer));
+          break;
+        case 3:
+          world.push(new Obstacle(j - 2, -i, data[index].obstacle));
+          break;
+        case 4:
+          world.push(new Obstacle(j - 2, -i, data[index].obstacle));
+          world.push(new Dreamcube(j - 2, -i, data[index].obstacle));
+          break;
+        case 5:
+          world.push(new Mat(j - 2, -i, data[index].mat));
+          world.push(new Skyobstacle(j - 2, -i, data[index].obstacle));
+          break;
+        case 6:
+          world.push(new Trskyobstacle(j - 2, -i, data[index].obstacle));
+          break;
+        case 7:
+          world.push(new Mat(j - 2, -i, data[index].mat));
+          world.push(new Mysterybox(j - 2, -i, data[index].bouncer));
+          break;
+        case 8:
+          world.push(new Mat(j - 2, -i, data[index].mat));
+          world.push(new Gem(j - 2, -i, data[index].bouncer));
+          break;
+        case 9:
+          world.push(new Goal(j - 2, -i, data[index].obstacle));
+      }
+    }
+  }
+}
+
+var ball = new Ball();
+
+keystate = [];
+//Loop function
+function render() {
+  renderer.render(scene, camera);
+  ball.update();
+  percent = Math.ceil(
+    Math.abs(ball.mesh.position.z) / data[level - 1].data.length * 100
+  );
+  percent = percent > 100 ? 100 : percent;
+  $('#percent').html(percent + '%');
+  if (keystate[37]) ball.mesh.position.x -= 0.15;
+  if (keystate[39]) ball.mesh.position.x += 0.15;
+  reqId = requestAnimationFrame(render);
+};
+
+//controls
+
+function gameover() {
+  started = false;
+  ball.speed.z = 0;
+  $('#main').fadeIn(500);
+  $('#retry').hide();
+  $('#play').show();
+  $('#play').click(start);
+  $('#level').show();
+  $('#stars').show();
+  $('#score').show();
+  $('#score').html($('#percent').html());
+  $('#main').css('pointer-events', 'auto');
+  if (level == 1) {
+      $('#ruby').each(function(){
+    this.pause(); // Stop playing
+    this.currentTime = 0; // Reset time
+});
+     } else if (level == 2) {
+      $('#rgw').each(function(){
+    this.pause(); // Stop playing
+    this.currentTime = 0; // Reset time
+});
+     } else if (level == 3) {
+      $('#dia').each(function(){
+    this.pause(); // Stop playing
+    this.currentTime = 0; // Reset time
+});
+     } else if (level == 4) {
+      $('#kya').each(function(){
+    this.pause(); // Stop playing
+    this.currentTime = 0; // Reset time
+});
+     } else {
+      $('#cloud').each(function(){
+    this.pause(); // Stop playing
+    this.currentTime = 0; // Reset time
+});
+     }
+}
