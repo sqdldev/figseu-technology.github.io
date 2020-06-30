7bccf6ccfce0c76daaaff942cd0724e9a2cf8c9b
FeX Uranus <66378306+FeX-Uranus@users.noreply.github.com>
2020-06-30 18:35:23 +0900

    Update main_Y.js

--- main_Y.js
+++ main_Y.js
@@ -24,7 +24,7 @@ let reqId;
   let stage_name = prompt("名前を決めてください");
   let stage_info = prompt("ステージの概要を決めてください");
   let stage_star = prompt("ステージの星の数を決めてください");
-  alert('ステージのデータを file:///sdcard/RSE/levels.json に配置。音楽のデータを file:///sdcard/music/BGM.mp3 に配置してください。');
+  alert('ステージのデータを読み込み、音楽のデータを file:///sdcard/music/BGM.mp3 に配置してください');
   $('#play').show();
   $('#play').click(start);
   $('#next').hide();
@@ -43,7 +43,6 @@ let reqId;
   console.log(
     "最終ステージ - The END　～遊んでくれてありがとう～"
   );
-});
 
 function fileload() {
   $.getJSON(ipt, d => {
