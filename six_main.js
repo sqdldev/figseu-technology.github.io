ae4cede9c25fd801a6909f0035f2aafae950d6ba
FeX Uranus <66378306+FeX-Uranus@users.noreply.github.com>
2020-07-29 12:53:00 +0900

    Update six_main.js

--- six_main.js
+++ six_main.js
@@ -141,7 +141,7 @@ function nextLevel() {
   $('#prev').show();
   $('#retry').hide();
   $('#play').show();
-  if (level == 3) {
+  if (level == 4) {
     $('#next').hide();
   }
   if (level == 1) {
