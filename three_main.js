827d94a09e7bd2133bd1c5788df70e9b26aa3a30
Figseu <66378306+FeX-Uranus@users.noreply.github.com>
2020-08-30 20:31:29 +0900

    Update three_main.js

--- three_main.js
+++ three_main.js
@@ -19,6 +19,13 @@ let scoreSubmitted = false;
 let level = 1;
 let star = 0;
 let wave = 0;
+let wave_t = 0;
+let wave_th = 0;
+let wave_f = 0;
+wave = $.cookie('th_wave');
+wave_t = $.cookie('th_wave_t');
+wave_th = $.cookie('th_wave_th');
+wave_f = $.cookie('th_wave_f');
 let url = location.href;
 let fgnc = url.substr( 68 );
 let data;
@@ -30,27 +37,10 @@ $.getJSON('QrJSs7WNhkQM4mkdWzwu8jGCtHVenGBz7VVEYG35FYwtF36TKt.json', d => {
   $('#play').click(start);
   $('#next').show();
   $('#score').hide();
-  if (level == 1) {
-         $('#level-d').html('Designed by ムー');
-         $('#level').html('スタジアム');
-         $('#stars').html('☆☆☆');
-        } else if (level == 2) {
-         $('#level-d').html('ボーナスステージ');
-         $('#level').html('戦国時代');
-         $('#stars').html('☆☆☆');
-        } else if (level == 3) {
-         $('#level-d').html('ボーナスステージ');
-         $('#level').html('滅びた文明');
-         $('#stars').html('☆☆☆');
-        } else if (level == 4) {
-         $('#level-d').html('6月限定ステージ');
-         $('#level').html('梅雨');
-         $('#stars').html('☆☆☆');
-        } else {
-            $('#level-d').html('開発中');
-            $('#level').html('Level ' + level);
-            $('#stars').html('');
-           }
+  $('#level-d').html('Designed by ムー');
+  $('#level').html('スタジアム');
+  $('#stars').html('☆☆☆');
+  $('#Waves').html('☆ × ' + $.cookie('th_wave'));
   reqId = requestAnimationFrame(render);
   console.clear();
   console.log(
@@ -66,15 +56,38 @@ function start(e) {
     ball.speed.z = -0.15;
     $('#main').fadeOut(300);
     $('#name').hide();
-    wave++;
+    if (typeof wave == 'undefined') {
+        wave = 0;
+        $.cookie('th_wave', wave, { expires: 30 });
+     }
+     if (typeof wave_t == 'undefined') {
+        wave_t = 0;
+        $.cookie('th_wave_t', wave_t, { expires: 30 });
+     }
+     if (typeof wave_th == 'undefined') {
+        wave_th = 0;
+        $.cookie('th_wave_th', wave_th, { expires: 30 });
+     }
+     if (typeof wave_f == 'undefined') {
+        wave_f = 0;
+        $.cookie('th_wave_f', wave_f, { expires: 30 });
+     }
     if (level == 1) {
         $('#stadium').get(0).play();
+        wave++;
+        $.cookie('th_wave', wave, { expires: 30 });
             } else if (level == 2) {
             $('#samurai').get(0).play();
+            wave_t++;
+            $.cookie('th_wave_t', wave_t, { expires: 30 });
             } else if (level == 3) {
             $('#fell').get(0).play();
+            wave_th++;
+            $.cookie('th_wave_th', wave_th, { expires: 30 });
             } else if (level == 4) {
             $('#rain').get(0).play();
+            wave_f++;
+            $.cookie('th_wave_f', wave_f, { expires: 30 });
             } else {
             $('#cloud').get(0).play();
             }
@@ -127,7 +140,6 @@ function reset() {
 function nextLevel() {
   percent = 0;
   star = 0;
-  wave = 0;
   while (
     (selectedObject = scene.getObjectByName('level component')) !== undefined
   ) {
@@ -150,18 +162,22 @@ function nextLevel() {
          $('#level-d').html('Designed by ムー');
          $('#level').html('スタジアム');
          $('#stars').html('☆☆☆');
+         $('#Waves').html('☆ × ' + $.cookie('th_wave'));
         } else if (level == 2) {
          $('#level-d').html('ボーナスステージ');
          $('#level').html('戦国時代');
          $('#stars').html('☆☆☆');
+         $('#Waves').html('☆ × ' + $.cookie('th_wave_t'));
         } else if (level == 3) {
          $('#level-d').html('ボーナスステージ');
          $('#level').html('滅びた文明');
          $('#stars').html('☆☆☆');
+         $('#Waves').html('☆ × ' + $.cookie('th_wave_th'));
         } else if (level == 4) {
          $('#level-d').html('Extra ステージ');
          $('#level').html('梅雨');
          $('#stars').html('☆☆☆');
+         $('#Waves').html('☆ × ' + $.cookie('th_wave_f'));
         } else {
             $('#level-d').html('開発中');
             $('#level').html('Level ' + level);
@@ -172,7 +188,6 @@ function nextLevel() {
 function prevLevel() {
   percent = 0;
   star = 0;
-  wave = 0;
   while (
     (selectedObject = scene.getObjectByName('level component')) != undefined
   ) {
@@ -195,18 +210,22 @@ function prevLevel() {
          $('#level-d').html('Designed by ムー');
          $('#level').html('スタジアム');
          $('#stars').html('☆☆☆');
+         $('#Waves').html('☆ × ' + $.cookie('th_wave'));
         } else if (level == 2) {
          $('#level-d').html('ボーナスステージ');
          $('#level').html('戦国時代');
          $('#stars').html('☆☆☆');
+         $('#Waves').html('☆ × ' + $.cookie('th_wave_t'));
         } else if (level == 3) {
          $('#level-d').html('ボーナスステージ');
          $('#level').html('滅びた文明');
          $('#stars').html('☆☆☆');
+         $('#Waves').html('☆ × ' + $.cookie('th_wave_th'));
         } else if (level == 4) {
          $('#level-d').html('Extra ステージ');
          $('#level').html('梅雨');
          $('#stars').html('☆☆☆');
+         $('#Waves').html('☆ × ' + $.cookie('th_wave_f'));
         } else {
             $('#level-d').html('開発中');
             $('#level').html('Level ' + level);
@@ -318,8 +337,20 @@ function render() {
 
 function gameover() {
   if (star == 1) {
-      wave = 0;
+     if (level == 1) {
+     wave = 0;
+     } else if (level == 2) {
+     wave_t = 0;
+     } else if (level == 3) {
+     wave_th = 0;
+     } else if (level == 4) {
+     wave_f = 0;
      }
+     $.cookie('th_wave', wave, { expires: 30 });
+     $.cookie('th_wave_t', wave_t, { expires: 30 });
+     $.cookie('th_wave_th', wave_th, { expires: 30 });
+     $.cookie('th_wave_f', wave_f, { expires: 30 });
+  }
   started = false;
   ball.speed.z = 0;
   $('#main').fadeIn(500);
@@ -332,11 +363,13 @@ function gameover() {
   $('#score').html($('#percent').html());
   $('#main').css('pointer-events', 'auto');
   if (level == 1) {
+      $('#Waves').html('☆ × ' + $.cookie('th_wave'));
       $('#stadium').each(function(){
     this.pause(); // Stop playing
     this.currentTime = 0; // Reset time
 });
      } else if (level == 2) {
+      $('#Waves').html('☆ × ' + $.cookie('th_wave_t'));
        if (star == 2) {
              $('#level-d').html('天下を掴め！');
       }
@@ -345,6 +378,7 @@ function gameover() {
     this.currentTime = 0; // Reset time
 });
      } else if (level == 3) {
+      $('#Waves').html('☆ × ' + $.cookie('th_wave_th'));
        if (star == 2) {
              $('#level-d').html('文明は甦る夢を見る');
       }
@@ -353,6 +387,7 @@ function gameover() {
     this.currentTime = 0; // Reset time
 });
      } else if (level == 4) {
+      $('#Waves').html('☆ × ' + $.cookie('th_wave_f'));
        if (star == 2) {
              $('#level-d').html('空に舞う雨のダンス');
       }
@@ -366,5 +401,4 @@ function gameover() {
     this.currentTime = 0; // Reset time
 });
      }
-   $('#Waves').html('☆ × ' + wave);
 }
