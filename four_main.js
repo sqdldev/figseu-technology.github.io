cbeab0e09486211731e09688dc6eb9a7b342c499
Figseu <66378306+FeX-Uranus@users.noreply.github.com>
2020-09-20 10:25:40 +0900

    Update four_main.js

--- four_main.js
+++ four_main.js
@@ -56,7 +56,7 @@ function start(e) {
   if (!started) {
     started = true;
     ball.speed.z = -0.15;
-    camera.rotation.x = 0;
+    camera.rotation.x = -0.75;
     $('#main').fadeOut(300);
     $('#name').hide();
     if (typeof wave == 'undefined') {
@@ -150,7 +150,7 @@ function reset() {
 function nextLevel() {
   percent = 0;
   star = 0;
-  camera.rotation.x = 0;
+  camera.rotation.x = -0.75;
   while (
     (selectedObject = scene.getObjectByName('level component')) !== undefined
   ) {
@@ -204,7 +204,7 @@ function nextLevel() {
 function prevLevel() {
   percent = 0;
   star = 0;
-  camera.rotation.x = 0;
+  camera.rotation.x = -0.75;
   while (
     (selectedObject = scene.getObjectByName('level component')) != undefined
   ) {
