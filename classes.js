f6a54861d1ca248db8cfbd131528c9a1f8860025
Figseu <66378306+FeX-Uranus@users.noreply.github.com>
2020-10-15 12:25:42 +0900

    Update classes.js

--- classes.js
+++ classes.js
@@ -1073,8 +1073,8 @@ class DaysCube {
     this.edgesGeometry = new THREE.EdgesGeometry(this.geometry);
     this.edgesMaterial = new THREE.LineBasicMaterial({ color: 0x000000 });
     this.line = new THREE.LineSegments(this.edgesGeometry, this.edgesMaterial);
-    this.mesh.position.set(xpos, 0.4, zpos);
-    this.line.position.set(xpos, 0.4, zpos);
+    this.mesh.position.set(xpos, 2.4, zpos);
+    this.line.position.set(xpos, 2.4, zpos);
     this.mesh.name = 'level component';
     this.line.name = 'level component';
     scene.add(this.line);
