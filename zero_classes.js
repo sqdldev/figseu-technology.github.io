072493721541d35c58ef1d9e7c9a0d85c35893a5
Figseu <66378306+FeX-Uranus@users.noreply.github.com>
2020-10-19 22:41:38 +0900

    Update zero_classes.js

--- zero_classes.js
+++ zero_classes.js
@@ -411,9 +411,9 @@ class Points {
     if (
       ball.mesh.position.x >= this.mesh.position.x - 10 &&
       ball.mesh.position.x <= this.mesh.position.x + 10 &&
-      ball.mesh.position.z >= this.mesh.position.z - 8.8 &&
-      ball.mesh.position.z <= this.mesh.position.z + 8.8 &&
-      ball.mesh.position.z <= 8.8 &&
+      ball.mesh.position.z >= this.mesh.position.z - 1.8 &&
+      ball.mesh.position.z <= this.mesh.position.z + 1.8 &&
+      ball.mesh.position.z <= 1.8 &&
       ball.mesh.position.y < this.mesh.position.y + 8
     ) return true;
   }
