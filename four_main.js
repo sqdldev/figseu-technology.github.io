67c7a625528aa751b0ba98b4ef198ca024a86697
Figseu Technology <66378306+Figseu-Technology@users.noreply.github.com>
2021-02-12 16:43:32 +0900

    Update four_main.js

--- four_main.js
+++ four_main.js
@@ -60,6 +60,9 @@ function start(e) {
     camera.rotation.y = 0;
     $('#main').fadeOut(300);
     $('#name').hide();
+    $('#jump').get(0).play();
+    $('#gem').get(0).play();
+    $('#speedup').get(0).play();
     if (typeof wave == 'undefined') {
         wave = 0;
         $.cookie('f_wave', wave, { expires: 30, domain:'figseu-technology.github.io', path:'/RollingSkyEvolution/'});
