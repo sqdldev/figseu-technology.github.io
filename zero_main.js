2ac8b0987cc121e5d443579e2f7032a8104c9858
Figseu Technology <66378306+Figseu-Technology@users.noreply.github.com>
2021-02-12 15:51:15 +0900

    Update zero_main.js

--- zero_main.js
+++ zero_main.js
@@ -53,6 +53,8 @@ function start(e) {
     ball.speed.z = -0.15;
     $('#main').fadeOut(300);
     $('#name').hide();
+    $('#jump').get(0).play();
+    $('#gem').get(0).play();
     if (typeof wave == 'undefined') {
         wave = 0;
         $.cookie('z_wave', wave, { expires: 40, domain:'figseu-technology.github.io', path:'/RollingSkyEvolution/'});
