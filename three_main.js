b815ceed4e65b8ce43906b4b8259929839ddbf52
Figseu Technology <66378306+Figseu-Technology@users.noreply.github.com>
2021-02-12 16:39:39 +0900

    Update three_main.js

--- three_main.js
+++ three_main.js
@@ -56,6 +56,9 @@ function start(e) {
     ball.speed.z = -0.15;
     $('#main').fadeOut(300);
     $('#name').hide();
+    $('#jump').get(0).play();
+    $('#gem').get(0).play();
+    $('#speedup').get(0).play();
     if (typeof wave == 'undefined') {
         wave = 0;
         $.cookie('th_wave', wave, { expires: 30, domain:'figseu-technology.github.io', path:'/RollingSkyEvolution/'});
