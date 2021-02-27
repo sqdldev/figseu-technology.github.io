b226d7ee27dcc748e3082373c92f2b0e8e28f92a
Figseu Technology <66378306+Figseu-Technology@users.noreply.github.com>
2021-02-27 21:37:21 +0900

    Update 9sg4DF2tU4KW3FNHAYrX6kVNdsx8ZzzMLRcm8mMVBEHDpgw5mm.js

--- 9sg4DF2tU4KW3FNHAYrX6kVNdsx8ZzzMLRcm8mMVBEHDpgw5mm.js
+++ 9sg4DF2tU4KW3FNHAYrX6kVNdsx8ZzzMLRcm8mMVBEHDpgw5mm.js
@@ -145,6 +145,17 @@ class Ball {
               this.tmpZ = 0;
             }
           }
+        } else if (v instanceof DebugMat || v instanceof Bouncer) {
+          if (v.detect()) {
+            this.landed = true;
+            this.speed.y = 0;
+            this.mesh.position.y = 0.6;
+            if (this.tmpZ) {
+              this.mesh.position.z = this.tmpZ - 4;
+              camera.position.z = this.tmpZ - 4 + distance;
+              this.tmpZ = 0;
+            }
+          }
         } else if (v instanceof Deltemat && fgnc == 'Function_---Eshell---_-Deltemat-_Landing-True-' || v instanceof Bouncer) {
           if (v.detect()) {
             this.landed = true;
@@ -1054,6 +1065,33 @@ class RandomMat {
       ball.mesh.position.x <= this.mesh.position.x + 0.8 &&
       ball.mesh.position.z >= this.mesh.position.z - 0.8 &&
       ball.mesh.position.z <= this.mesh.position.z + 0.8 &&
+      ball.mesh.position.z <= 1
+    ) return true;
+  }
+}
+
+class DebugMat {
+  constructor(xpos, zpos, color) {
+    color = parseInt(color);
+    this.geometry = new THREE.BoxGeometry(1, 0.2, 1);
+    this.material = new THREE.MeshPhongMaterial({ color: color });
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.edgesGeometry = new THREE.EdgesGeometry(this.geometry);
+    this.edgesMaterial = new THREE.LineBasicMaterial({ color: color });
+    this.line = new THREE.LineSegments(this.edgesGeometry, this.edgesMaterial);
+    this.mesh.position.set(xpos, 0, zpos);
+    this.line.position.set(xpos, 0, zpos);
+    this.mesh.name = 'level component';
+    this.line.name = 'level component';
+    scene.add(this.line);
+
+  }
+  detect() {
+    if (
+      ball.mesh.position.x >= this.mesh.position.x - 1000 &&
+      ball.mesh.position.x <= this.mesh.position.x + 1000 &&
+      ball.mesh.position.z >= this.mesh.position.z - 1000 &&
+      ball.mesh.position.z <= this.mesh.position.z + 1000 &&
       ball.mesh.position.z <= 1
     ) return true;
   }
