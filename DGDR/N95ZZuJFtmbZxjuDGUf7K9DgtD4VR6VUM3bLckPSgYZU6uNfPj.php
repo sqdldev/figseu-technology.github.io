9c71cadc4c24e0151fa5ef6695769e469cc9ef6b
Figseu Technology <66378306+Figseu-Technology@users.noreply.github.com>
2021-04-02 18:56:19 +0900

    Create N95ZZuJFtmbZxjuDGUf7K9DgtD4VR6VUM3bLckPSgYZU6uNfPj.php

---
+++ DGDR/N95ZZuJFtmbZxjuDGUf7K9DgtD4VR6VUM3bLckPSgYZU6uNfPj.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<title>Rolling Sky Evolution - D.G.D.R</title>
+        <link rel="canonical" href="https://figseu-technology.github.io/RollingSkyEvolution/">
+        <meta property="twitter:card" content="summary_large_image">
+	<meta name="robots" content="noindex">
+        <meta property="og:url" content="https://figseu-technology.github.io/RollingSkyEvolution/">
+        <meta property="og:site_name" content="Rolling Sky Evolution - D.G.D.R">
+        <meta property="og:title" content="Rolling Sky Evolution">
+        <meta property="og:description" content="...">
+        <meta property="description" content="...">
+        <meta property="og:image" content="https://figseu-technology.github.io/RollingSkyEvolution/img/icon.png">
+        <meta name="keywords" content="ゲーム,難しい,ローリングスカイ,rolling sky,楽しい,無料,スマホ,アクション,世界一難しいゲーム,人間卒業">
+	<link rel="stylesheet" href="eight_style.css">
+	<link href="css.css" rel="stylesheet">
+	<link rel="icon" href="https://figseu-technology.github.io/RollingSkyEvolution/img/icon.png">
+        <link rel="shortcut icon" href="https://figseu-technology.github.io/RollingSkyEvolution/img/icon.png">
+        <link rel="apple-touch-icon" href="https://figseu-technology.github.io/RollingSkyEvolution/img/icon.png">
+  <audio rel="preload" src="https://figseu-technology.github.io/RollingSkyEvolution/se/jump.mp3" id="jump" preload="auto"></audio>
+  <audio rel="preload" src="https://figseu-technology.github.io/RollingSkyEvolution/se_A/clickdown.mp3" id="cd_1" preload="auto"></audio>
+  <audio rel="preload" src="https://figseu-technology.github.io/RollingSkyEvolution/se_A/Maya_cd.mp3" id="cd_2" preload="auto"></audio>
+  <audio rel="preload" src="https://figseu-technology.github.io/RollingSkyEvolution/se_A/Hina_cd.mp3" id="cd_3" preload="auto"></audio>
+  <audio rel="preload" src="https://figseu-technology.github.io/RollingSkyEvolution/se/Gem.mp3" id="gem" preload="auto"></audio>
+  <audio rel="preload" src="https://figseu-technology.github.io/RollingSkyEvolution/bgm/Maison.mp3" id="mai" preload="auto"></audio>
+</head>
+<body onContextmenu=”return false”>
+  <div id="canvascontainer">
+    <div id="percent">0%</div>
+  </div>
+  <div id="main">
+    <div id="level"></div>
+    <div id="score">D.G.D.R</div>
+    <div id="stars"></div>
+        <div id="level-d"></div>
+        <a href="index.html" id="one_level">▷</a>
+        <div id="Waves" onclick="cl()">☆ × 0</div>
+        <div id="PresentCode">➣</div>
+    <div class="button" id="play">➣</div>
+    <div class="button" id="retry">
+      <img id="retry-icon" src="img/arrow-right.svg">
+    </div>
+    <div class="button" id="next" onclick="nextLevel()">▷</div>
+    <div class="button" id="prev" onclick="prevLevel()">◁</div>
+  </div>
+  <script src="https://figseu-technology.github.io/RollingSkyEvolution/System/three.js"></script>
+	<script src="https://figseu-technology.github.io/RollingSkyEvolution/System/jquery.js"></script>
+        <script src="https://figseu-technology.github.io/RollingSkyEvolution/System/cookie.js"></script>
+	<script src="https://figseu-technology.github.io/RollingSkyEvolution/9sg4DF2tU4KW3FNHAYrX6kVNdsx8ZzzMLRcm8mMVBEHDpgw5mm.js"></script>
+        <script src="N9_M_5ZZuJFtmbZxjuDGUf7K9DgtD4VR6VUM3bLckPSgYZU6uNfPj.js"></script>
+	<script src="https://figseu-technology.github.io/RollingSkyEvolution/System/controls.js"></script>
+</body>
+</html>
