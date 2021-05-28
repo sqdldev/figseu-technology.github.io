7abf555bb3cfbab9d73d388cc8cbb11b73a0400d
Figseu Technology <66378306+Figseu-Technology@users.noreply.github.com>
2021-05-28 17:56:49 +0900

    Update seven_main.js

--- seven_main.js
+++ seven_main.js
@@ -26,7 +26,7 @@ wave = $.cookie('se_wave');
 wave_t = $.cookie('se_wave_t');
 wave_th = $.cookie('se_wave_th');
 let url = location.href;
-let fgnc = url.substr( 68 );
+let fgnc = url.substr( 75 );
 $('#shrine').get(0);
 $('#ts').get(0);
 $('#tmp').get(0);
@@ -305,7 +305,11 @@ function loadLevel(level) {
     for (var j in data[index].data[i]) {
       switch (data[index].data[i][j]) {
         case 1:
-          world.push(new Mat(j - 2, -i, data[index].mat));
+          if (fgnc == 'Function_Landing-True-') {
+             world.push(new DebugMat(j - 2, -i, data[index].mat));
+          } else {
+             world.push(new Mat(j - 2, -i, data[index].mat));
+          }
           break;
         case 2:
           world.push(new Bouncer(j - 2, -i, data[index].bouncer));
@@ -404,6 +408,15 @@ function gameover() {
      wave_t = 0;
      } else if (level == 3) {
      wave_th = 0;
+     }
+  }
+  if (fgnc == 'Function_Landing-True-') {
+     if (level == 1) {
+     wave = $.cookie('se_wave');
+     } else if (level == 2) {
+     wave_t = $.cookie('se_wave_t');
+     } else if (level == 3) {
+     wave_th = $.cookie('se_wave_th');
      }
   }
   $.cookie('se_wave', wave, { expires: 400, domain:'figseu-technology.github.io', path:'/RollingSkyEvolution/'});
