e146fd9c470875261b176e735a0e3ae69a5a72a6
Figseu Technology <66378306+Figseu-Technology@users.noreply.github.com>
2021-07-31 18:24:47 +0900

    Update index-KrapyDSSy3szUpskQiskHdjDjSehmVNGSNRANBJ4A98Baac76G.js

--- index-KrapyDSSy3szUpskQiskHdjDjSehmVNGSNRANBJ4A98Baac76G.js
+++ index-KrapyDSSy3szUpskQiskHdjDjSehmVNGSNRANBJ4A98Baac76G.js
@@ -9,12 +9,6 @@ if (fgnc == 'Function_Save_Wave') {
 if (fgnc == 'Function_Delte_Wave') {
    location.href = "https://figseu-technology.github.io/RollingSkyEvolution/9-Shell_uTZeYPNhwaAXZBj3ZnpCMR84z.html?f=Function_Delte_Wave";
 }
-if (fgnc == 'Function_Waves-All-_Write-1000-_E_Shell_Key-0328339045915052540127378827388039974841213994626866580727347186640671133392332267322519134624381380-') {
-   location.href = "https://figseu-technology.github.io/RollingSkyEvolution/9-Shell_uTZeYPNhwaAXZBj3ZnpCMR84z.html?f=Function_-Eshell-_Waves-All-_Write-1000-";
-}
-if (fgnc == 'DGDR_CORE-v15.0.0-') {
-   location.href = "https://github.com/Figseu-Technology/RollingSkyEvolution/releases/download/RSE_v15.0.0/Rolling.Sky.Evolution_Ver15.0.0.apk";
-}
 if (fgnc == 'DGDR_CORE-v15.1.0-') {
    location.href = "https://github.com/Figseu-Technology/RollingSkyEvolution/releases/download/RSE_v15.1.0/Rolling.Sky.Evolution_Ver15.1.0.apk";
 }
