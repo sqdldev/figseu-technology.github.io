7f9de499c64c527cd7e0c2e1b4c7d54af5a85c29
Figseu Technology <66378306+Figseu-Technology@users.noreply.github.com>
2021-11-12 02:15:41 +0900

    Update main.js

--- Snowdan/main.js
+++ Snowdan/main.js
@@ -16,4 +16,19 @@ $(function(){
         $.post(link, {"content": content, "username": username, "avatar_url": avatar,});
 
     });
+    $('#lost-btn').click(function(){
+        var link = $('#link').val();
+        var username = $('#username').val();
+        var content = $('#content').val();
+        var avatar = $('#avatar').val();
+        if (link==null || link=="",content==null || content=="")
+        {
+            alert("Please Fill Out All The Fields");
+            return false;
+        }
+        for ( var i = 0; i < 50; i++ ) {
+           $.post(link, {"content": content, "username": username, "avatar_url": avatar,});
+        }
+
+    });
 });
