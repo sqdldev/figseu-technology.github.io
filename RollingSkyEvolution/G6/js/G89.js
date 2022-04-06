7c11ae987acc81bc9731421e5c3d2c082c439263
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2022-04-06 17:41:27 +0900

    Create G89.js

---
+++ RollingSkyEvolution/G6/js/G89.js
@@ -0,0 +1,577 @@
+const LIGHTPOOLN = [
+    [15,'Aulick','Normal'],
+    [86,'Beagle','Normal'],
+    [87,'Bulldog','Normal'],
+    [5,'Cassin','Normal'],
+    [88,'Comet','Normal'],
+    [8,'Craven','Normal'],
+    [89,'Crescent','Normal'],
+    [90,'Cygnet','Normal'],
+    [6,'Downes','Normal'],
+    [16,'Foote','Normal'],
+    [91,'Foxhound','Normal'],
+    [238,'Knigsberg','Normal'],
+    [239,'Karlsruhe','Normal'],
+    [240,'Kln','Normal'],
+    [272,'Kisaragi','Normal'],
+    [104,'Leander','Normal'],
+    [9,'McCall','Normal'],
+    [271,'Mutsuki','Normal'],
+    [182,'Nagara','Normal'],
+    [29,'Omaha','Normal'],
+    [30,'Raleigh','Normal'],
+    [17,'Spence','Normal'],
+    [274,'Uzuki','Normal'],
+]
+const LIGHTPOOLR = [
+    [82,'Acasta','Rare'],
+    [105,'Achilles','Rare'],
+    [106,'Ajax','Rare'],
+    [81,'Amazon','Rare'],
+    [334,'Arashio','Rare'],
+    [83,'Ardent','Rare'],
+    [175,'Ariake','Rare'],
+    [331,'Asashio','Rare'],
+    [34,'Atlanta','Rare'],
+    [407,'Bache','Rare'],
+    [31,'Brooklyn','Rare'],
+    [366,'Concord','Rare'],
+    [372,'Curacoa','Rare'],
+    [373,'Curlew','Rare'],
+    [11,'Fletcher','Rare'],
+    [348,'Forbin','Rare'],
+    [277,'Fumizuki','Rare'],
+    [423,'Glasgow','Rare'],
+    [427,'Halsey_Powell','Rare'],
+    [318,'Hamakaze','Rare'],
+    [27,'Hammann','Rare'],
+    [171,'Hatsuharu','Rare'],
+    [174,'Hatsushimo','Rare'],
+    [365,'Hazelwood','Rare'],
+    [362,'Hobby','Rare'],
+    [317,'Isokaze','Rare'],
+    [328,'Jamaica','Rare'],
+    [307,'Jersey','Rare'],
+    [35,'Juneau','Rare'],
+    [306,'Jupiter','Rare'],
+    [363,'Kalk','Rare'],
+    [269,'Kamikaze','Rare'],
+    [374,'Kimberly','Rare'],
+    [293,'Kiyonami','Rare'],
+    [351,'Le_Mars','Rare'],
+    [270,'Matsukaze','Rare'],
+    [360,'Memphis','Rare'],
+    [332,'Ooshio','Rare'],
+    [32,'Phoenix','Rare'],
+    [417,'Smalley','Rare'],
+    [110,'Southampton','Rare'],
+    [409,'Stanly','Rare'],
+    [319,'Tanikaze','Rare'],
+    [316,'Urakaze','Rare'],
+    [173,'Wakaba','Rare'],
+    [176,'Yuugure','Rare'],
+    [346,'Z18','Rare'],
+    [264,'Z19','Rare'],
+    [263,'Bailey','Rare'],
+    [18,'Benson','Rare'],
+    [333,'Michishio','Rare'],
+]
+
+const LIGHTPOOLE = [
+    [118,'Aurora','Elite'],
+    [428,'Biloxi','Elite'],
+    [408,'Black_Prince','Elite'],
+    [255,'Chang_Chun','Elite'],
+    [391,'Clevelad','Elite'],
+    [330,'Denver','Elite'],
+    [151,'Fubuki','Elite'],
+    [113,'Gloucester','Elite'],
+    [296,'Harutsuki','Elite'],
+    [101,'Javelin','Elite'],
+    [19,'Laffey','Elite'],
+    [359,'Le_Tmraire','Elite'],
+    [390,'Lena','Elite'],
+    [335,'Little_Bel','Elite'],
+    [395,'LOpinitre','Elite'],
+    [325,'Matchless','Elite'],
+    [189,'Mikuma','Elite'],
+    [188,'Mogami','Elite'],
+    [375,'Mullany','Elite'],
+    [258,'Ning_Hai','Elite'],
+    [170,'Nowaki','Elite'],
+    [259,'Ping_Hai','Elite'],
+    [111,'Sheffield','Elite'],
+    [305,'St_Louis','Elite'],
+    [256,'Tai_Yuan','Elite'],
+    [257,'Yat_Sen','Elite'],
+    [297,'Yoizuki','Elite'],
+    [237,'Z25','Elite'],
+    [345,'Z35','Elite'],
+    [155,'Ayanami','Elite'],
+    [37,'Cleveland','Elite'],
+    [376,'Chaser','Elite'],
+    [33,'Helena','Elite'],
+]
+
+const LIGHTPOOLSR = [
+    [232,'Akashi','SuperRare'],
+    [262,'Avrora','SuperRare'],
+    [115,'Belfast','SuperRare'],
+    [107,'Dido','SuperRare'],
+    [288,'Kawakaze','SuperRare'],
+    [347,'Le_Triomphant','SuperRare'],
+    [394,'Le_Malin','SuperRare'],
+    [329,'Montpelier','SuperRare'],
+    [36,'San_Diego','SuperRare'],
+    [371,'Sirius','SuperRare'],
+    [393,'Swiftsure','SuperRare'],
+    [166,'Yukikaze','SuperRare'],
+    [267,'Z46','SuperRare'],
+]
+
+const HEAVYPOOLN = [
+    [192,'Aoba','Normal'],
+    [240,'Kln','Normal'],
+    [238,'Knigsberg','Normal'],
+    [239,'Karlsruhe','Normal'],
+    [193,'Kinugasa','Normal'],
+    [104,'Leander','Normal'],
+    [52,'Nevada','Normal'],
+    [53,'Oklahoma','Normal'],
+    [29,'Omaha','Normal'],
+    [39,'Pensacola','Normal'],
+    [30,'Raleigh','Normal'],
+]
+
+const HEAVYPOOLR = [
+    [106,'Ajax','Rare'],
+    [34,'Atlanta','Rare'],
+    [59,'California','Rare'],
+    [42,'Chicago','Rare'],
+    [210,'Ise','Rare'],
+    [121,'Kent','Rare'],
+    [123,'Norfolk','Rare'],
+    [41,'Northampton','Rare'],
+    [54,'Pennsylvania','Rare'],
+    [32,'Phoenix','Rare'],
+    [128,'Repulse','Rare'],
+    [120,'Shropshire','Rare'],
+    [122,'Suffolk','Rare'],
+    [337,'Sussex','Rare'],
+    [58,'Tennessee','Rare'],
+    [211,'Hyuuga','Rare'],
+    [44,'Portland','Rare'],
+    [241,'Leipzig','Rare'],
+]
+
+const HEAVYPOOLE = [
+    [336,'Abercrombie','Elite'],
+    [242,'Admiral_Hipper','Elite'],
+    [246,'Admiral_Graf_Spee','Elite'],
+    [55,'Arizona','Elite'],
+    [198,'Ashigara','Elite'],
+    [60,'Colorado','Elite'],
+    [245,'Deutschland','Elite'],
+    [124,'Dorsetshire','Elite'],
+    [352,'Dunkerque','Elite'],
+    [149,'Erebus','Elite'],
+    [126,'Exeter','Elite'],
+    [205,'Hiei','Elite'],
+    [383,'Hiei-chan','Elite'],
+    [43,'Houston','Elite'],
+    [45,'Indianapolis','Elite'],
+    [207,'Kirishima','Elite'],
+    [119,'London','Elite'],
+    [61,'Maryland','Elite'],
+    [132,'Nelson','Elite'],
+    [130,'Queen_Elizabeth','Elite'],
+    [127,'Renown','Elite'],
+    [133,'Rodney','Elite'],
+    [382,'Suzuya','Elite'],
+    [150,'Terror','Elite'],
+    [125,'York','Elite']
+]
+
+const HEAVYPOOLSR = [
+    [201,'Atago','SuperRare'],
+    [136,'Duke_of_York','SuperRare'],
+    [129,'Hood','SuperRare'],
+    [353,'Jean_Bart','SuperRare'],
+    [354,'Massachusetts','SuperRare'],
+    [320,'Mikasa','SuperRare'],
+    [364,'Minneapolis','SuperRare'],
+    [212,'Nagato','SuperRare'],
+    [63,'North_Carolina','SuperRare'],
+    [135,'Prince_of_Wales','SuperRare'],
+    [244,'Prinz_Eugen','SuperRare'],
+    [200,'Takao','SuperRare'],
+    [251,'Tirpitz','SuperRare'],
+    [131,'Warspite','SuperRare'],
+    [64,'Washington','SuperRare']
+]
+
+const SPECIALPOOLN = [
+    [192,'Aoba','Normal'],
+    [71,'Bogue','Normal'],
+    [140,'Hermes','Normal'],
+    [240,'Kln','Normal'],
+    [238,'Knigsberg','Normal'],
+    [239,'Karlsruhe','Normal'],
+    [193,'Kinugasa','Normal'],
+    [72,'Langley','Normal'],
+    [104,'Leander','Normal'],
+    [29,'Omaha','Normal'],
+    [39,'Pensacola','Normal'],
+    [30,'Raleigh','Normal'],
+    [75,'Ranger','Normal']
+]
+
+const SPECIALPOOLR = [
+    [106,'Ajax','Rare'],
+    [34,'Atlanta','Rare'],
+    [42,'Chicago','Rare'],
+    [121,'Kent','Rare'],
+    [70,'Long_Island','Rare'],
+    [123,'Norfolk','Rare'],
+    [41,'Northampton','Rare'],
+    [32,'Phoenix','Rare'],
+    [120,'Shropshire','Rare'],
+    [122,'Suffolk','Rare'],
+    [44,'Portland','Rare'],
+    [241,'Leipzig','Rare']
+]
+
+const SPECIALPOOLE = [
+    [384,'Akagi-chan','Elite'],
+    [144,'Ark_Royal','Elite'],
+    [376,'Chaser','Elite'],
+    [342,'Dace','Elite'],
+    [148,'Glorious','Elite'],
+    [78,'Hornet','Elite'],
+    [220,'Houston','Elite'],
+    [45,'Indianapolis','Elite'],
+    [396,'I-25','Elite'],
+    [339,'I-26','Elite'],
+    [397,'I-56','Elite'],
+    [340,'I-58','Elite'],
+    [146,'Victorious','SuperRare'],
+    [73,'Lexington','Elite'],
+    [80,'Vestal','Elite'],
+    [119,'London','Elite'],
+    [74,'Saratoga','Elite'],
+    [350,'Surcouf','Elite'],
+    [400,'U-522','Elite'],
+    [344,'U-557','Elite'],
+    [142,'Unicorn','Elite'],
+    [49,'Wichita','Elite'],
+    [76,'Yorktown','Elite'],
+    [385,'Zeppy','Elite']
+]
+
+const SPECIALPOOLSR = [
+    [356,'Centaur','SuperRare'],
+    [77,'Enterprise','SuperRare'],
+    [357,'Essex','SuperRare'],
+    [381,'I-13','SuperRare'],
+    [398,'I-168','SuperRare'],
+    [338,'I-19','SuperRare'],
+    [378,'Shangri-La','SuperRare'],
+    [228,'Shoukaku','SuperRare'],
+    [399,'U-101','SuperRare'],
+    [343,'U-47','SuperRare'],
+    [341,'U-81','SuperRare'],
+    [146,'Victorious','SuperRare'],
+    [229,'Zuikaku','SuperRare'],
+    [252,'Graf_Zeppelin','SuperRare'],
+    [145,'Illustrious','SuperRare'],
+    [230,'Taihou','SuperRare'],
+    [0,'New_Jersey','UltraRare'],
+    [0,'Shinano','UltraRare']
+]
+
+const NONEVENTPOOLE = [
+    [144,'Ark_Royal','Elite'],
+    [78,'Hornet','Elite'],
+    [220,'Houston','Elite'],
+    [45,'Indianapolis','Elite'],
+    [73,'Lexington','Elite'],
+    [80,'Vestal','Elite'],
+    [119,'London','Elite'],
+    [74,'Saratoga','Elite'],
+    [142,'Unicorn','Elite'],
+    [76,'Yorktown','Elite'],
+    [55,'Arizona','Elite'],
+    [149,'Erebus','Elite'],
+    [126,'Exeter','Elite'],
+    [132,'Nelson','Elite'],
+    [127,'Renown','Elite'],
+    [133,'Rodney','Elite'],
+    [150,'Terror','Elite'],
+    [125,'York','Elite']
+]
+
+const NONEVENTPOOLSR = [
+    [77,'Enterprise','SuperRare'],
+    [357,'Essex','SuperRare'],
+    [378,'Shangri-La','SuperRare'],
+    [145,'Illustrious','SuperRare'],
+    [201,'Atago','SuperRare'],
+    [129,'Hood','SuperRare'],
+    [135,'Prince_of_Wales','SuperRare'],
+    [244,'Prinz_Eugen','SuperRare'],
+    [200,'Takao','SuperRare'],
+    [131,'Warspite','SuperRare']
+]
+
+const EVENTPOOLR = [
+    [999,'Marblehead','Rare'],      //5%
+]
+
+const EVENTPOOLE = [
+    [999,'Cooper','Elite'],         //2.5%
+    [999,'Nautilus','Elite'],       //2.5%5%
+]
+
+const EVENTPOOLSR = [
+    [999,'Intrepid','SuperRare'],   //2%
+    [999,'Reno','SuperRare'],       //2%
+    [999,'Ingraham','SuperRare'],   //2%
+    [999,'Bremerton','SuperRare'],   //0.5%
+]
+
+let rareRate = 10;
+let eliteRate = 5;
+let srRate = 4;
+
+let shipsNum = document.querySelector('#shipsNum');
+let totalCoins = 0;
+let totalCubes = 0;
+
+const banner = document.querySelector('#banner');
+const pool = document.getElementsByClassName('btn-check');
+
+
+document.querySelector('#btnradio1').addEventListener('click', function(){
+    if (pool[0].checked === true){
+        banner.src = 'media/imgs/eventBanner.png';
+    }
+});
+
+document.querySelector('#btnradio2').addEventListener('click', function(){
+    if (pool[1].checked === true){
+        banner.src = 'media/imgs/lightBanner.png';
+    }
+});
+
+document.querySelector('#btnradio3').addEventListener('click', function(){
+    if (pool[2].checked === true){
+        banner.src = 'media/imgs/heavyBanner.png';
+    }
+});
+
+document.querySelector('#btnradio4').addEventListener('click', function(){
+    if (pool[3].checked === true){
+        banner.src = 'media/imgs/specialBanner.png';
+    }
+});
+
+document.querySelector('#resetCounters').addEventListener('click', function(){
+    totalCoins = 0;
+    totalCubes = 0;
+    document.querySelector('#coinsAmount').innerHTML = totalCoins;
+    document.querySelector('#cubesAmount').innerHTML = totalCubes;
+});
+
+
+
+/* CONSTRUCCTION RATES 
+ * Super Rare 7%
+ * Elite 12%
+ * Rare 51%
+ * Common 30%
+ * 
+ * Super Rare Focused 2%
+ * Super Rare Exchange Focused 0.5%
+ * Elite Focused 2.5%
+ * Elite Exchange Focused 2.5%
+ * Rare Focused 5%
+*/
+
+
+document.querySelector('#buildBtn').addEventListener('click', function(){
+
+    let parrafo = true;
+    let ordenes = document.getElementById('orders');
+    let child = ordenes.lastElementChild;
+
+    
+    if(pool[1].checked === true){
+        totalCoins = totalCoins + (1 * shipsNum.value);
+        totalCubes = totalCubes + (0 * shipsNum.value);
+        document.querySelector('#coinsAmount').innerHTML = totalCoins;
+        document.querySelector('#cubesAmount').innerHTML = totalCubes;
+    }else{
+        totalCoins = totalCoins + (1 * shipsNum.value);
+        totalCubes = totalCubes + (0 * shipsNum.value);
+        document.querySelector('#coinsAmount').innerHTML = totalCoins;
+        document.querySelector('#cubesAmount').innerHTML = totalCubes;
+    }
+
+
+
+    while (child){
+        ordenes.removeChild(child);
+        child = ordenes.lastElementChild;
+    }
+
+    for (let i = 0; i<shipsNum.value; i++){
+        
+        if (i > 4 && parrafo){ 
+            document.querySelector('#orders').appendChild(document.createElement('p'));
+            parrafo = false;
+        }
+
+        if (pool[1].checked === true){
+            building(i, LIGHTPOOLN, LIGHTPOOLR, LIGHTPOOLE, LIGHTPOOLSR, false);
+        }else if(pool[2].checked === true){
+            building(i, HEAVYPOOLN, HEAVYPOOLR, HEAVYPOOLE, HEAVYPOOLSR, false);
+        }else if(pool[3].checked === true){
+            building(i, SPECIALPOOLN, SPECIALPOOLR, SPECIALPOOLE, SPECIALPOOLSR, false);
+        }else{
+            building(i, SPECIALPOOLN, HEAVYPOOLR.concat(SPECIALPOOLR), NONEVENTPOOLE, NONEVENTPOOLSR, true);
+        }
+    }
+})
+
+function building(i, poolN, poolR, poolE, poolSR, isEvent){
+    let ship = document.createElement('img');
+    let shipLabel = document.createElement('label');
+    let division = document.createElement('div');
+    let division2 = document.createElement('div');
+    let shipData = buildingShip(poolN, poolR, poolE, poolSR, isEvent);
+    let shipName = shipData[1];
+    let shipRarity = shipData[2];
+
+    ship.src = `media/imgs/ships/70px-${shipName}Icon.png`;
+    division.className = shipRarity
+    division.id = shipName + i;
+    division2.id = shipName + i + 'd';
+    division2.style = 'font-size:11px; width:70px; background-color: #000B3F; color: white; text-align: center; white-space: nowrap; overflow: clip;'
+    shipLabel.textContent = shipName;
+    document.querySelector('#orders').appendChild(division);
+    document.querySelector('#'+ shipName + i).appendChild(ship);
+    document.querySelector('#'+ shipName + i).appendChild(division2);
+    document.querySelector('#'+ shipName + i + 'd').appendChild(shipLabel);
+}
+
+function random(cant){
+    let number = Math.round(Math.random() * cant);
+    return number;
+}
+
+function buildingShip(normal, rare, elite, sr, isEvent){
+
+    let shipId;
+    let rndNumber = random(200);
+    let rates = [102, 60, 20, 10]; //normal, rare, elite, sr
+
+    //Common
+    if (rndNumber <= rates[0]){
+        shipId = random(normal.length - 1);
+        return normal[shipId]; 
+    }
+
+    //Rare
+    rndNumber = rndNumber - rates[0];
+    if (rndNumber <= rates[1]){
+        if (isEvent){
+            return eventShip(rare, EVENTPOOLR, rareRate, rndNumber);
+        }else{
+            shipId = random(rare.length - 1);
+            return rare[shipId];
+        }
+    }
+
+    //Elite
+    rndNumber = rndNumber - rates[1];
+
+    if (rndNumber <= rates[2]){
+        if (isEvent){
+            return eventShip(elite, EVENTPOOLE, eliteRate, rndNumber);
+        }else{
+            shipId = random(elite.length - 1);
+            return elite[shipId];
+        }
+    
+    }else{
+
+        rndNumber = rndNumber - rates[2];
+        if (isEvent){
+            return eventShip(sr, EVENTPOOLSR, srRate, rndNumber);
+        }
+        shipId = random(sr.length - 1);
+        return sr[shipId];
+    }
+
+}
+
+function eventShip(normalBanner, eventBanner, rate, randomNumber){
+
+    let wonEventShip = true;
+
+    if (randomNumber > (eventBanner.length * rate) - 3){
+        wonEventShip = false;
+    }
+
+    if (wonEventShip){
+        for (i = 0; i <= eventBanner.length; i++){
+            if (randomNumber <= rate){
+                return eventBanner[i];
+            }
+            randomNumber = randomNumber - rate;
+            console.log('randomNumber[' + i + '] = ' + randomNumber)
+        }
+
+    }else{
+
+        shipId = random(normalBanner.length - 1);
+        return normalBanner[shipId];
+
+    }
+}
+
+
+function countItem(pool){
+
+    totalItems = 0;
+
+    if (pool.length < 0){
+        return 0;
+    }
+
+    for (i = 0; i < pool.length; i++){
+            totalItems += 1;
+    }
+
+    return totalItems;
+}
+
+// Test functions ********************************************************
+
+
+function testPoolShips(poolSelect){
+    for (let i = Number(0); i<poolSelect.length; i++){
+        let ship = document.createElement('img');
+        //console.log(`[${i}-${poolSelect[i][0]}] - ${poolSelect[i][1]} length=${poolSelect.length}`);
+        ship.src = `media/imgs/ships/70px-${poolSelect[i][1]}Icon.png`;
+        ship.className = poolSelect[i][2];
+        document.querySelector('#orders').appendChild(ship);
+    }
+}
+
+function testOneShip(shipName, rarity){
+    let ship = document.createElement('img');
+    ship.src = `media/imgs/ships/70px-${shipName}Icon.png`;
+    ship.className = rarity;
+    document.querySelector('#orders').appendChild(ship);
+}
