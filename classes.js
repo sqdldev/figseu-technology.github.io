4776a90f72c24d8e367a40c49932d578194563b2
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2022-05-15 09:58:59 +0900

    Update classes.js

--- classes.js
+++ classes.js
@@ -1304,7 +1304,7 @@ class SkyCastle_B {
     this.edgesMaterial = new THREE.LineBasicMaterial({ color: 0xC8A600 });
     this.line = new THREE.LineSegments(this.edgesGeometry, this.edgesMaterial);
     this.mesh.position.set(xpos, 1.7, zpos);
-    this.mesh.rotation.set(-0.2, 0, 0);
+    this.mesh.rotation.set(-0.5, 0, 0);
     this.line.position.set(xpos, -10, zpos);
     this.mesh.name = 'level component';
     this.line.name = 'level component';
@@ -1332,7 +1332,7 @@ class SkyCastle_Y {
     this.edgesMaterial = new THREE.LineBasicMaterial({ color: 0xC8A600 });
     this.line = new THREE.LineSegments(this.edgesGeometry, this.edgesMaterial);
     this.mesh.position.set(xpos, 1.7, zpos);
-    this.mesh.rotation.set(-0.2, 0, 0);
+    this.mesh.rotation.set(-0.5, 0, 0);
     this.line.position.set(xpos, -10, zpos);
     this.mesh.name = 'level component';
     this.line.name = 'level component';
