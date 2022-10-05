7923f3553e2b0aff65bb15c7761b9d0cd705f2c1
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2022-10-05 16:54:02 +0900

    Create Ax9P7rS49pP3ZRKrdY5GFKNk2CbnPs.js

---
+++ Ax9P7rS49pP3ZRKrdY5GFKNk2CbnPs.js
@@ -0,0 +1,9 @@
+let n_wave_s = 0;
+let gds = Math.floor( Math.random() * (10000-99999)+99999 );
+n_wave_s = $.cookie('n_wave_s');
+$('#c-power').html('立線エネルギー : ' + n_wave_s);
+if (n_wave_s >= 1) {
+   $('#mirror').html('<img src="https://thisanimedoesnotexist.ai/results/psi-1.0/seed' + gds + '.png"></img>');
+   n_wave_s--
+   $.cookie('n_wave_s', n_wave_s, { expires: 400, domain:'figseu-technology.github.io'});
+}
