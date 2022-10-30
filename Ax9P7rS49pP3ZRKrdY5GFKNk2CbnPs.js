3b0779f91bfa0b6f478b2bbe609e7ed5b17a9e10
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2022-10-31 02:51:38 +0900

    Update Ax9P7rS49pP3ZRKrdY5GFKNk2CbnPs.js

--- Ax9P7rS49pP3ZRKrdY5GFKNk2CbnPs.js
+++ Ax9P7rS49pP3ZRKrdY5GFKNk2CbnPs.js
@@ -14,17 +14,20 @@ let gds = Math.floor( Math.random() * (10000-99999)+99999 );
 n_wave_s = $.cookie('n_wave_s');
 $('#c-power').html('立線エネルギー : ' + n_wave_s);
 if (n_wave_s >= 1) {
-   $('#mirror').html('<img src="https://thisanimedoesnotexist.ai/results/psi-1.0/seed' + gds + '.png"></img>');
+   $('#mirror').html('<a href="https://thisanimedoesnotexist.ai/results/psi-1.0/seed' + gds + '.png" download="Erosion.jpeg"><img src="https://thisanimedoesnotexist.ai/results/psi-1.0/seed' + gds + '.png"></img></a>');
    n_wave_s--
    Pfd_100 = 999;
    $.cookie('n_wave_s', n_wave_s, { expires: 400, domain:'figseu-technology.github.io'});
    $.cookie('Pfd_100', Pfd_100, { expires: 400, domain:'figseu-technology.github.io'});
+   $('#c-power').html('侵食値 : ' + gds + 'LP');
    if (gds == 99057) {
       Pfd_101 = 999;
       $.cookie('Pfd_101', Pfd_101, { expires: 400, domain:'figseu-technology.github.io'});
+      $('#c-power').html('新たな実績を解除しました。');
    } else if (gds == 50157) {
       Pfd_102 = 999;
       $.cookie('Pfd_102', Pfd_102, { expires: 400, domain:'figseu-technology.github.io'});
+      $('#c-power').html('新たな実績を解除しました。');
    } else if (gds == 53241) {
       Pfd_103 = 999;
       $.cookie('Pfd_103', Pfd_103, { expires: 400, domain:'figseu-technology.github.io'});
@@ -40,11 +43,14 @@ if (n_wave_s >= 1) {
    } else if (gds == 69751) {
       Pfd_107 = 999;
       $.cookie('Pfd_107', Pfd_107, { expires: 400, domain:'figseu-technology.github.io'});
+      $('#c-power').html('新たな実績を解除しました。');
    } else if (gds == 69861) {
       Pfd_108 = 999;
       $.cookie('Pfd_108', Pfd_108, { expires: 400, domain:'figseu-technology.github.io'});
+      $('#c-power').html('新たな実績を解除しました。');
    } else if (gds == 13621) {
       Pfd_109 = 999;
       $.cookie('Pfd_109', Pfd_109, { expires: 400, domain:'figseu-technology.github.io'});
+      $('#c-power').html('新たな実績を解除しました。');
    }
 }
