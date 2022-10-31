0ef88d06c8a8de84bcd928a3ab9d7e1ea239f147
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2022-11-01 01:35:25 +0900

    Update Ax9P7rS49pP3ZRKrdY5GFKNk2CbnPs.js

--- Ax9P7rS49pP3ZRKrdY5GFKNk2CbnPs.js
+++ Ax9P7rS49pP3ZRKrdY5GFKNk2CbnPs.js
@@ -1,4 +1,4 @@
-let n_wave_s = 0;
+let f_wave = 0;
 let Pfd_100 = 0;
 let Pfd_101 = 0;
 let Pfd_102 = 0;
@@ -11,13 +11,13 @@ let Pfd_108 = 0;
 let Pfd_109 = 0;
 let Pfd_110 = 0;
 let gds = Math.floor( Math.random() * (10000-99999)+99999 );
-n_wave_s = $.cookie('n_wave_s');
-$('#c-power').html('立線エネルギー : ' + n_wave_s);
-if (n_wave_s >= 1) {
+f_wave = $.cookie('f_wave');
+$('#c-power').html('立線エネルギー : ' + f_wave);
+if (f_wave >= 1) {
    $('#mirror').html('<img src="https://thisanimedoesnotexist.ai/results/psi-1.0/seed' + gds + '.png"></img>');
-   n_wave_s--
+   f_wave--
    Pfd_100 = 999;
-   $.cookie('n_wave_s', n_wave_s, { expires: 400, domain:'figseu-technology.github.io'});
+   $.cookie('f_wave', f_wave, { expires: 400, domain:'figseu-technology.github.io'});
    $.cookie('Pfd_100', Pfd_100, { expires: 400, domain:'figseu-technology.github.io'});
    $('#c-power').html('侵食値 : ' + gds + 'LP');
    if (gds == 99057) {
