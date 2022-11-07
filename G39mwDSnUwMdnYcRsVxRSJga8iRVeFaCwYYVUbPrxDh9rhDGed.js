da5f5f26dae08e7d313ceccf284b470e8345e884
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2022-11-08 03:58:32 +0900

    Update G39mwDSnUwMdnYcRsVxRSJga8iRVeFaCwYYVUbPrxDh9rhDGed.js

--- G39mwDSnUwMdnYcRsVxRSJga8iRVeFaCwYYVUbPrxDh9rhDGed.js
+++ G39mwDSnUwMdnYcRsVxRSJga8iRVeFaCwYYVUbPrxDh9rhDGed.js
@@ -1,12 +1,13 @@
-let f_wave = 0;
+let te_wave_s = 0;
 let alter = 0;
 let gds = Math.floor( Math.random() * 2001);
-f_wave = $.cookie('f_wave');
-$('#c-power').html('立線エネルギー : ' + f_wave);
+te_wave_s = $.cookie('te_wave_s');
+$('#c-power').html('立線エネルギー : ' + te_wave_s);
 
-if (f_wave >= 1) {
+if (te_wave_s >= 1) {
    $('#mirror').html('<img src="https://m6.hentaiera.com/020/w90al63to2/' + gds + '.jpg"></img>');
-   //f_wave--
-   $.cookie('f_wave', f_wave, { expires: 400, domain:'figseu-technology.github.io'});
+   te_wave_s--
+   $.cookie('te_wave_s', te_wave_s, { expires: 400, domain:'figseu-technology.github.io'});
+   gds = gds * 6547;
    $('#c-power').html('終渡値 : ' + gds + 'LP');
 }
