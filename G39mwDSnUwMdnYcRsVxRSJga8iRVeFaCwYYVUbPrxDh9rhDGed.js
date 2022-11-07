d9b076699e7ba88edb610f2241407af93fc2c18c
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2022-11-08 02:14:58 +0900

    Update G39mwDSnUwMdnYcRsVxRSJga8iRVeFaCwYYVUbPrxDh9rhDGed.js

--- G39mwDSnUwMdnYcRsVxRSJga8iRVeFaCwYYVUbPrxDh9rhDGed.js
+++ G39mwDSnUwMdnYcRsVxRSJga8iRVeFaCwYYVUbPrxDh9rhDGed.js
@@ -1,11 +1,11 @@
 let f_wave = 0;
 let alter = 0;
-let gds = Math.floor( Math.random() * 36901 ) + 213000;
+let gds = Math.floor( Math.random() * 2001);
 f_wave = $.cookie('f_wave');
 $('#c-power').html('立線エネルギー : ' + f_wave);
 
 if (f_wave >= 1) {
-   $('#mirror').html('<img src="http://www.hentairider.com/media/images/4/top-hentai-anime/top-hentai-anime-' + gds + '.jpg"></img>');
+   $('#mirror').html('<img src="https://m6.hentaiera.com/020/w90al63to2/' + gds + '.jpg"></img>');
    //f_wave--
    $.cookie('f_wave', f_wave, { expires: 400, domain:'figseu-technology.github.io'});
    $('#c-power').html('終渡値 : ' + gds + 'LP');
