3c60ca5d4db675072cb1f112f50b1669cd4337a2
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2022-12-03 19:05:28 +0900

    Update index-KrapyDSSy3szUpskQiskHdjDjSehmVNGSNRANBJ4A98Baac76G.js

--- index-KrapyDSSy3szUpskQiskHdjDjSehmVNGSNRANBJ4A98Baac76G.js
+++ index-KrapyDSSy3szUpskQiskHdjDjSehmVNGSNRANBJ4A98Baac76G.js
@@ -1,16 +1,16 @@
 let url = location.href;
-let fgnc = url.substr( 69 );
+let fgnc = url.substr( 49 );
 if (fgnc == '3RFxSK8TwMFKHPrTAXC3GCjmEKAAiNceW62Nf92aXT9AZpbsXg') {
    
 }
 if (fgnc == 'Function_Save_Wave') {
-   location.href = "https://figseu-technology.github.io/RollingSkyEvolution/9-Shell_uTZeYPNhwaAXZBj3ZnpCMR84z.html?f=Function_Save_Wave";
+   location.href = "https://figseu-technology.github.io/9-Shell_uTZeYPNhwaAXZBj3ZnpCMR84z.html?f=Function_Save_Wave";
 }
 if (fgnc == 'Function_Delte_Wave') {
-   location.href = "https://figseu-technology.github.io/RollingSkyEvolution/9-Shell_uTZeYPNhwaAXZBj3ZnpCMR84z.html?f=Function_Delte_Wave";
+   location.href = "https://figseu-technology.github.io/9-Shell_uTZeYPNhwaAXZBj3ZnpCMR84z.html?f=Function_Delte_Wave";
 }
-if (fgnc == 'DGDR_CORE-v15.1.0-') {
-   location.href = "https://github.com/Figseu-Technology/RollingSkyEvolution/releases/download/RSE_v15.1.0/Rolling.Sky.Evolution_Ver15.1.0.apk";
+if (fgnc == 'DGDR_CORE-v3.0.0E-') {
+   location.href = "https://github.com/Figseu-Technology/figseu-technology.github.io/releases/download/RSE_vE3.0/RollingSkyEvolution_V3.0E.apk";
 }
 if (fgnc == 'RSE_Editor') {
    location.href = "https://github.com/Figseu-Technology/RollingSkyEvolution/releases/download/RSE_v15.1.0/RSE.Editor_Ver3.0.0.apk";
@@ -19,16 +19,16 @@ if (fgnc == 'Function_LeaderBoard') {
    location.href = "https://mee6.xyz/leaderboard/846657424890658846";
 }
 if (fgnc == 'Function_Join_RollingSkyEvolution') {
-   location.href = "https://discord.gg/duBXgTRKZE";
+   location.href = "https://discord.gg/TaJ9f9PAsW";
 }
 if (fgnc == 'DGDR_CORE-7DA_8m5x24SkYeV4E8bbKuUdXktHK7fNFp3LQzRpTVMdK4yGrgbaNc5LZUySyzARXnMyU3SxPP4J3QC-') {
-   location.href = "https://figseu-technology.github.io/RollingSkyEvolution/seven_index.html?f=7DA_8m5x24SkYeV4E8bbKuUdXktHK7fNFp3LQzRpTVMdK4yGrgbaNc5LZUySyzARXnMyU3SxPP4J3QC";
+   location.href = "https://figseu-technology.github.io/seven_index.html?f=7DA_8m5x24SkYeV4E8bbKuUdXktHK7fNFp3LQzRpTVMdK4yGrgbaNc5LZUySyzARXnMyU3SxPP4J3QC";
 }
 if (fgnc == 'DGDR_CORE-9MZ_RsYg22xBCSeZPyTK5GsC2EQ3J7Ku2BKJjs4w3RWd5gSrGPjQbiP9p4SUPb5Q2MrxLNUETEfCVLc-') {
-   location.href = "https://figseu-technology.github.io/RollingSkyEvolution/9-Shell_uTZeYPNhwaAXZBj3ZnpCMR84z.html?f=9MZ_RsYg22xBCSeZPyTK5GsC2EQ3J7Ku2BKJjs4w3RWd5gSrGPjQbiP9p4SUPb5Q2MrxLNUETEfCVLc";
+   location.href = "https://figseu-technology.github.io/9-Shell_uTZeYPNhwaAXZBj3ZnpCMR84z.html?f=9MZ_RsYg22xBCSeZPyTK5GsC2EQ3J7Ku2BKJjs4w3RWd5gSrGPjQbiP9p4SUPb5Q2MrxLNUETEfCVLc";
 }
 if (fgnc == 'DGDR_CORE-BLYK10_AgKeBiptdNeeWjr3MTmMhuDHYwjUEfNGmB47UuXs3bR55cXYPWDY6WtBnhUywY5GZXf9kDWVCh4pNAXiHNUjiZgLn8npS8Mg4cFh-') {
-   location.href = "https://figseu-technology.github.io/RollingSkyEvolution/ten_index.html?f=BLYK10_AgKeBiptdNeeWjr3MTmMhuDHYwjUEfNGmB47UuXs3bR55cXYPWDY6WtBnhUywY5GZXf9kDWVCh4pNAXiHNUjiZgLn8npS8Mg4cFh";
+   location.href = "https://figseu-technology.github.io/ten_index.html?f=BLYK10_AgKeBiptdNeeWjr3MTmMhuDHYwjUEfNGmB47UuXs3bR55cXYPWDY6WtBnhUywY5GZXf9kDWVCh4pNAXiHNUjiZgLn8npS8Mg4cFh";
 }
 
 if( window.canRunAds === undefined ){
