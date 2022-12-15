a0312d765e3e65301a98e9f8f69af6d7d33905f4
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2022-12-15 22:14:51 +0900

    Update CcPXXRPMZaVma4necJyByXyK.js

--- CcPXXRPMZaVma4necJyByXyK.js
+++ CcPXXRPMZaVma4necJyByXyK.js
@@ -1,14 +1,7 @@
 let Master_Icon = 0;
-let Luncher = 0;
 
 function Submit() {
-   Luncher = $('#avatar_url').val();
-   Luncher = Luncher.includes('.jpg');
-   if (Luncher = true) {
-      Master_Icon = $('#avatar_url').val();
-      $.cookie('Master_Icon', Master_Icon, { expires: 500, domain:'figseu-technology.github.io'});
-      location.href = "https://figseu-technology.github.io/Profile.html";
-   } else {
-      location.href = "https://figseu-technology.github.io/AvatarEdit.html";
-   }
+   Master_Icon = $('#avatar_url').val();
+   $.cookie('Master_Icon', Master_Icon, { expires: 400, domain:'figseu-technology.github.io'});
+   location.href = "https://figseu-technology.github.io/Profile.html";
 }
