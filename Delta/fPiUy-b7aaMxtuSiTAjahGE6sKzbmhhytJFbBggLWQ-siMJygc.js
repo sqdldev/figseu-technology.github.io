e8b1e0bf873c235880bb183c763a4e179e36eebe
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2022-12-21 16:39:00 +0900

    Create fPiUy-b7aaMxtuSiTAjahGE6sKzbmhhytJFbBggLWQ-siMJygc.js

---
+++ Delta/fPiUy-b7aaMxtuSiTAjahGE6sKzbmhhytJFbBggLWQ-siMJygc.js
@@ -0,0 +1,10 @@
+let Stages = 0;
+
+function Submit() {
+   Stages = $('#stage-select').val();
+   if (Stages == "Time Crystal") {
+      location.href = "https://figseu-technology.github.io/Delta/Delta1_u36WTVFd8jpG64.html";
+   } else {
+      location.href = "https://figseu-technology.github.io/Delta/";
+   }
+}
