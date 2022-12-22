9a2c8477ab4fbb92f11610311c84a09342090ae2
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2022-12-23 03:24:24 +0900

    Add files via upload

---
+++ FC Grown/Grown.js
@@ -0,0 +1,25 @@
+function G() {
+let A = prompt("url")
+var greet = document.getElementById("greet")
+greet.innerHTML = "<h3><a href='" + A + "' download='file'>HTTPS</a></h3><br> <br/>"
+}
+function D() {
+let B = prompt("url")
+var verow = document.getElementById("verow")
+verow.innerHTML = "<h3><a href='" + B + "' download='file'>HTTP</a></h3><br> <br/>"
+}
+function R() {
+let C = prompt("image url")
+var agreat = document.getElementById("agreat")
+agreat.innerHTML = "<a href='" + C + "'><img class='ip' alt='' src='C'></img></a>"
+}
+function U() {
+let D = prompt("Video url")
+var wender = document.getElementById("wender")
+wender.innerHTML = "<video src='" + D + "' controls loop poster='file:///android_asset/FeX_Uranus_Music/images/FU.jpg'></video>"
+}
+function Y() {
+let E = prompt("Video ID");
+var youigreat = document.getElementById("youigreat")
+youigreat.innerHTML = "<iframe id='downloder' src='https://www.youtube.com/embed/" + E +"' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+}
