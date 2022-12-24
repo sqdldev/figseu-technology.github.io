584b6c8f7bdc134fa710b6eee408d56d37989156
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2022-12-25 00:38:23 +0900

    Update fPiUy-b7aaMxtuSiTAjahGE6sKzbmhhytJFbBggLWQ-siMJygc.js

--- Delta/fPiUy-b7aaMxtuSiTAjahGE6sKzbmhhytJFbBggLWQ-siMJygc.js
+++ Delta/fPiUy-b7aaMxtuSiTAjahGE6sKzbmhhytJFbBggLWQ-siMJygc.js
@@ -4,6 +4,8 @@ function Submit() {
    Stages = $('#stage-select').val();
    if (Stages == "Time Crystal") {
       location.href = "https://figseu-technology.github.io/Delta/Delta1_u36WTVFd8jpG64.html";
+   } else if (Stages == "The Almandine") {
+      location.href = "https://figseu-technology.github.io/Delta/Delta3_j5iR3Ny5ipd9cfD5FQQVH.html";
    } else {
       location.href = "https://figseu-technology.github.io/Delta/";
    }
