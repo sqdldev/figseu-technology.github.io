4e4468f63314e0ae02548e5e8bbced602a7fd312
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2022-12-30 13:12:06 +0900

    Update Delta1_u36WTVFd8jpG64.js

--- Delta/Delta1_u36WTVFd8jpG64.js
+++ Delta/Delta1_u36WTVFd8jpG64.js
@@ -246,7 +246,7 @@ scene.add(light);
 const world = [];
 function loadLevel(level) {
   const index = level - 1;
-  renderer.THREE.ImageUtils.loadTexture( 'textures/163_20221230123545.cmr' );
+  renderer.setClearColor(parseInt(0x1ffffff));
   ball.mesh.material.color.setHex(parseInt(data[index].ball));
   scene.fog = new THREE.Fog(parseInt(data[index].background), 10, 30);
   for (var i in data[index].data) {
