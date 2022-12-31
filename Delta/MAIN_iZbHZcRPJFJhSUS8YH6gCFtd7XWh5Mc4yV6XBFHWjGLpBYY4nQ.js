eb32685fe1e73fbf76973d54fc9156e162571b52
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2022-12-31 22:40:05 +0900

    Update MAIN_iZbHZcRPJFJhSUS8YH6gCFtd7XWh5Mc4yV6XBFHWjGLpBYY4nQ.js

--- Delta/MAIN_iZbHZcRPJFJhSUS8YH6gCFtd7XWh5Mc4yV6XBFHWjGLpBYY4nQ.js
+++ Delta/MAIN_iZbHZcRPJFJhSUS8YH6gCFtd7XWh5Mc4yV6XBFHWjGLpBYY4nQ.js
@@ -1,6 +1,6 @@
 const grid = $('#grid');
 
-let gridLength = 20;
+let gridLength = 3000;
 let mouseDown = false;
 
 let UserName = 0;
@@ -30,7 +30,7 @@ $('#color').on('click', e => {
   $('#chenge_c').get(0).play();
   e.preventDefault();
   e.handled = true;
-	color = (color + 1) % 24;
+	color = (color + 1) % 93;
 	if (color == 1) {
 		$('#color_type').html('通常床');
 	} else if (color == 2) {
@@ -79,6 +79,142 @@ $('#color').on('click', e => {
 		$('#color_type').html('せり出すブロック3');
 	} else if (color == 24) {
 		$('#color_type').html('せり出すブロック3 (横連結)');
+	} else if (color == 25) {
+		$('#color_type').html('せり出すブロック1 (左移動)');
+	} else if (color == 26) {
+		$('#color_type').html('せり出すブロック1 (右移動)');
+	} else if (color == 27) {
+		$('#color_type').html('左へ移動する床');
+	} else if (color == 28) {
+		$('#color_type').html('右へ移動する床');
+	} else if (color == 29) {
+		$('#color_type').html('高い木');
+	} else if (color == 30) {
+		$('#color_type').html('高い木2');
+	} else if (color == 31) {
+		$('#color_type').html('高い木 (左移動)');
+	} else if (color == 32) {
+		$('#color_type').html('高い木 (右移動)');
+	} else if (color == 33) {
+		$('#color_type').html('ヤシの木');
+	} else if (color == 34) {
+		$('#color_type').html('ヤシの木2');
+	} else if (color == 35) {
+		$('#color_type').html('ヤシの木 (左移動)');
+	} else if (color == 36) {
+		$('#color_type').html('ヤシの木 (右移動)');
+	} else if (color == 37) {
+		$('#color_type').html('上方向接続禁止');
+	} else if (color == 38) {
+		$('#color_type').html('下方向接続禁止');
+	} else if (color == 39) {
+		$('#color_type').html('左方向接続禁止');
+	} else if (color == 40) {
+		$('#color_type').html('右方向接続禁止');
+	} else if (color == 41) {
+		$('#color_type').html('左に転がる玉');
+	} else if (color == 42) {
+		$('#color_type').html('右に転がる玉');
+	} else if (color == 43) {
+		$('#color_type').html('左に転がる玉2');
+	} else if (color == 44) {
+		$('#color_type').html('右に転がる玉2');
+	} else if (color == 45) {
+		$('#color_type').html('浮かぶ星');
+	} else if (color == 46) {
+		$('#color_type').html('浮かぶ星2');
+	} else if (color == 47) {
+		$('#color_type').html('浮かぶ星 (空中)');
+	} else if (color == 48) {
+		$('#color_type').html('不明');
+	} else if (color == 49) {
+		$('#color_type').html('大型の電波塔');
+	} else if (color == 50) {
+		$('#color_type').html('大型の電波塔2');
+	} else if (color == 51) {
+		$('#color_type').html('ゲート');
+	} else if (color == 52) {
+		$('#color_type').html('ゲート2');
+	} else if (color == 53) {
+		$('#color_type').html('ゲート (閉)');
+	} else if (color == 54) {
+		$('#color_type').html('ゲート2 (閉)');
+	} else if (color == 55) {
+		$('#color_type').html('レーザー (縦｜赤)');
+	} else if (color == 56) {
+		$('#color_type').html('レーザー (横｜赤)');
+	} else if (color == 57) {
+		$('#color_type').html('レーザー (縦｜青)');
+	} else if (color == 58) {
+		$('#color_type').html('レーザー (横｜青)');
+	} else if (color == 59) {
+		$('#color_type').html('上下方向接続禁止');
+	} else if (color == 60) {
+		$('#color_type').html('左右方向接続禁止');
+	} else if (color == 61) {
+		$('#color_type').html('稼働床 (上)');
+	} else if (color == 62) {
+		$('#color_type').html('稼働床 (下)');
+	} else if (color == 63) {
+		$('#color_type').html('稼働床 (左)');
+	} else if (color == 64) {
+		$('#color_type').html('稼働床 (右)');
+	} else if (color == 65) {
+		$('#color_type').html('連動稼働床 (上)');
+	} else if (color == 66) {
+		$('#color_type').html('連動稼働床 (下)');
+	} else if (color == 67) {
+		$('#color_type').html('連動稼働床 (左)');
+	} else if (color == 68) {
+		$('#color_type').html('連動稼働床 (右)');
+	} else if (color == 69) {
+		$('#color_type').html('連動稼働床 (上｜連結なし)');
+	} else if (color == 70) {
+		$('#color_type').html('連動稼働床 (下｜連結なし)');
+	} else if (color == 71) {
+		$('#color_type').html('連動稼働床 (左｜連結なし)');
+	} else if (color == 72) {
+		$('#color_type').html('連動稼働床 (右｜連結なし)');
+	} else if (color == 73) {
+		$('#color_type').html('連動稼働床 (上｜左連結)');
+	} else if (color == 74) {
+		$('#color_type').html('連動稼働床 (下｜右連結)');
+	} else if (color == 75) {
+		$('#color_type').html('連動稼働床 (左｜右連結)');
+	} else if (color == 76) {
+		$('#color_type').html('連動稼働床 (右｜左連結)');
+	} else if (color == 77) {
+		$('#color_type').html('スイッチ (赤)');
+	} else if (color == 78) {
+		$('#color_type').html('スイッチ (青)');
+	} else if (color == 79) {
+		$('#color_type').html('レーザー発射装置');
+	} else if (color == 80) {
+		$('#color_type').html('下がるブロック');
+	} else if (color == 81) {
+		$('#color_type').html('宝石');
+	} else if (color == 82) {
+		$('#color_type').html('宝石2');
+	} else if (color == 83) {
+		$('#color_type').html('宝石3');
+	} else if (color == 84) {
+		$('#color_type').html('宝石4');
+	} else if (color == 85) {
+		$('#color_type').html('宝石5');
+	} else if (color == 86) {
+		$('#color_type').html('宝石 (ジャンプ床)');
+	} else if (color == 87) {
+		$('#color_type').html('宝石 (左移動)');
+	} else if (color == 88) {
+		$('#color_type').html('宝石 (右移動)');
+	} else if (color == 89) {
+		$('#color_type').html('左に動くブロック');
+	} else if (color == 90) {
+		$('#color_type').html('右に動くブロック');
+	} else if (color == 91) {
+		$('#color_type').html('左に動くブロック2');
+	} else if (color == 92) {
+		$('#color_type').html('右に動くブロック2');
 	} else if (color == 0) {
 		$('#color_type').html('空白');
 	} else {
