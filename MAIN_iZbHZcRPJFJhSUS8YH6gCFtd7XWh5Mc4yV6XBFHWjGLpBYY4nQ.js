b00258d588cef0ed93bb651277b67d0992b5ccb8
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2023-01-06 08:11:30 +0900

    Update MAIN_iZbHZcRPJFJhSUS8YH6gCFtd7XWh5Mc4yV6XBFHWjGLpBYY4nQ.js

--- MAIN_iZbHZcRPJFJhSUS8YH6gCFtd7XWh5Mc4yV6XBFHWjGLpBYY4nQ.js
+++ MAIN_iZbHZcRPJFJhSUS8YH6gCFtd7XWh5Mc4yV6XBFHWjGLpBYY4nQ.js
@@ -19,7 +19,7 @@ for (let i = 0; i < gridLength; i++) {
 
 $('#designer').scrollTop($('#designer')[0].scrollHeight);
 
-const colors = ['#000B3F', '#FFD300', '#FF7E00', '#7E7E7E', '#FFB9E1', '#002CFF', '#B900A4', '#C4B500', '#00A548', '#FFFFFF', '#FF000A', '#00FFC5', '#00C5FF', '#E77CA9', '#E88CFF', '#9F4800', '#00FFE2', '#FFAFA5', '#4C3D00', '#FAFFA2', '#BABABA', '#285400'];
+const colors = , , , ];
 var color = 1;
 
 $('#color').on('click', e => {
@@ -33,54 +33,91 @@ $('#color').on('click', e => {
 	color = $('#tile-select').val();
 	if (color == 1) {
 		$('#color_type').html('通常床');
+                colors = '#FFD300';
 	} else if (color == 2) {
 		$('#color_type').html('ジャンプ台');
+                colors = '#FF7E00';
 	} else if (color == 3) {
 		$('#color_type').html('ブロック');
+                colors = '#7E7E7E';
 	} else if (color == 4) {
 		$('#color_type').html('ドリームキューブ');
+                colors = '#FFB9E1';
 	} else if (color == 5) {
 		$('#color_type').html('トンネル');
+                colors = '#002CFF';
 	} else if (color == 6) {
 		$('#color_type').html('高いトンネル');
+                colors = '#B900A4';
 	} else if (color == 7) {
 		$('#color_type').html('王冠');
+                colors = '#C4B500';
 	} else if (color == 8) {
 		$('#color_type').html('宝石');
+                colors = '#00A548';
 	} else if (color == 9) {
 		$('#color_type').html('ゴール');
+                colors = '#FFFFFF';
 	} else if (color == 10) {
 		$('#color_type').html('スピードアップ');
+                colors = '#FF000A';
 	} else if (color == 11) {
 		$('#color_type').html('エフェクトクリア');
+                colors = '#00FFC5';
 	} else if (color == 12) {
 		$('#color_type').html('スピードダウン');
+                colors = '#00C5FF';
 	} else if (color == 13) {
 		$('#color_type').html('進行方向反転');
+                colors = '#E77CA9';
 	} else if (color == 14) {
 		$('#color_type').html('ワープホール');
+                colors = '#E88CFF';
 	} else if (color == 15) {
 		$('#color_type').html('ジャンプ床(加速時)');
+                colors = '#9F4800';
 	} else if (color == 16) {
 		$('#color_type').html('高いブロック');
+                colors = '#00FFE2';
 	} else if (color == 17) {
 		$('#color_type').html('タワーブロック');
+                colors = '#FFAFA5';
 	} else if (color == 18) {
 		$('#color_type').html('通行不可の床');
+                colors = '#4C3D00';
 	} else if (color == 19) {
 		$('#color_type').html('通行可能ブロック');
+                colors = '#FAFFA2';
 	} else if (color == 20) {
 		$('#color_type').html('落ちる床');
+                colors = '#BABABA';
 	} else if (color == 21) {
 		$('#color_type').html('Function mat');
+                colors = '#285400';
 	} else if (color == 22) {
 		$('#color_type').html('Function Block');
+                colors = '#285400';
 	} else if (color == 23) {
-		$('#color_type').html('Delte Obstacle S');
+		$('#color_type').html('Almandine Crystal');
+                colors = '#FFCE37';
+	} else if (color == 38) {
+		$('#color_type').html('視点を右に傾ける');
+                colors = '#FF677F';
+	} else if (color == 39) {
+		$('#color_type').html('視点を左に傾ける');
+                colors = '#5191E7';
 	} else if (color == 50) {
-		$('#color_type').html('Sky Obstacle N');
+		$('#color_type').html('1段階浮いたブロック');
+                colors = '#005DFF';
+	} else if (color == 76) {
+		$('#color_type').html('浮遊するブロック');
+                colors = '#95FFB4';
+	} else if (color == 210) {
+		$('#color_type').html('ドリームタワー');
+                colors = '#962974';
 	} else if (color == 0) {
 		$('#color_type').html('空白');
+                colors = '#000B3F';
 	} else {
 		$('#color_type').html('オブジェクトエラー');
 	}
