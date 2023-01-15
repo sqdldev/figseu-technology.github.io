5a32a47250759b84c74676569381397fb5369eb9
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2023-01-15 09:44:13 +0900

    Update fPiUy-b7aaMxtuSiTAjahGE6sKzbmhhytJFbBggLWQ-siMJygc.js

--- Delta/fPiUy-b7aaMxtuSiTAjahGE6sKzbmhhytJFbBggLWQ-siMJygc.js
+++ Delta/fPiUy-b7aaMxtuSiTAjahGE6sKzbmhhytJFbBggLWQ-siMJygc.js
@@ -6,6 +6,8 @@ function Submit() {
       location.href = "https://figseu-technology.github.io/Delta/Delta1_u36WTVFd8jpG64.html";
    } else if (Stages == "The Almandine") {
       location.href = "https://figseu-technology.github.io/Delta/Delta3_j5iR3Ny5ipd9cfD5FQQVH.html";
+   } else if (Stages == "The Obsidian") {
+      location.href = "https://figseu-technology.github.io/Delta/Delta60_6Yaj6fWSr5wJJznWEjHy224.html";
    } else {
       location.href = "https://figseu-technology.github.io/Delta/";
    }
