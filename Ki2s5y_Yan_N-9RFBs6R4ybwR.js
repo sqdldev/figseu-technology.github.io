7a6d2d8503ba1b213be9a9444fa63758d6590619
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2023-01-17 11:13:01 +0900

    Update Ki2s5y_Yan_N-9RFBs6R4ybwR.js

--- Ki2s5y_Yan_N-9RFBs6R4ybwR.js
+++ Ki2s5y_Yan_N-9RFBs6R4ybwR.js
@@ -1,15 +1,19 @@
 let UserName = 0;
 let Password = 0;
+let RE_SaveData = 0;
 UserName = $.cookie('UserName');
 Password = $.cookie('Password');
-if (typeof UserName !== 'undefined' || typeof Password !== 'undefined') {
+RE_SaveData = $.cookie('RE_SaveData');
+if (typeof UserName !== 'undefined' && typeof Password !== 'undefined' && typeof RE_SaveData !== 'undefined') {
    location.href = "https://figseu-technology.github.io/";
 }
 
 function Submit() {
    UserName = $('#name').val();
    Password = $('#pass').val();
+   RE_SaveData = $('#gmail_name').val();
    $.cookie('UserName', UserName, { expires: 600, domain:'figseu-technology.github.io'});
    $.cookie('Password', Password, { expires: 600, domain:'figseu-technology.github.io'});
+   $.cookie('RE_SaveData', RE_SaveData, { expires: 600, domain:'figseu-technology.github.io'});
    location.href = "https://figseu-technology.github.io/";
 }
